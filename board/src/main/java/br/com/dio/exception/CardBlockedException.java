d29edc5ff54bb895074afa7b9e05fb6740be4c3f
s1ngk <gabrielkellyone@gmail.com>
2025-06-08 20:33:16 -0300

    Adicionado projeto Board de Tarefas

---
+++ board/src/main/java/br/com/dio/exception/CardBlockedException.java
@@ -0,0 +1,5 @@
+package br.com.dio.exception;
+
+public class CardBlockedException extends RuntimeException{
+    public CardBlockedException(final String message) { super(message); }
+}
---
+++ board/src/main/java/br/com/dio/service/BoardService.java
@@ -0,0 +1,52 @@
+package br.com.dio.service;
+
+import br.com.dio.persistence.dao.BoardColumnDAO;
+import br.com.dio.persistence.dao.BoardDAO;
+import br.com.dio.persistence.entity.BoardEntity;
+import lombok.AllArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@AllArgsConstructor
+public class BoardService {
+
+    private final Connection connection;
+
+    public BoardEntity insert(final BoardEntity entity) throws SQLException {
+        var boardDao = new BoardDAO(connection);
+        var boardColumnDao = new BoardColumnDAO(connection);
+        try {
+            boardDao.insert(entity);
+            var columns = entity.getBoardColumns().stream().map(c -> {
+                c.setBoard(entity);
+                return c;
+            }).toList();
+            for (var column : columns) {
+                boardColumnDao.insert(column);
+            }
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+            throw e;
+        }
+        return entity;
+    }
+
+    public boolean delete(final Long id) throws SQLException {
+
+        var dao = new BoardDAO(connection);
+        try {
+            if (!dao.exists(id)) {
+                return false;
+            }
+            dao.delete(id);
+            connection.commit();
+            return true;
+        } catch (SQLException e) {
+            connection.rollback();
+            throw e;
+        }
+
+    }
+}
